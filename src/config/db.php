409fe69a7ee2212799ec42a8e0e46988b5f7626f
tannergr <tanner@tlgraham.com>
2018-03-02 00:03:00 -0800

    move db stuff

--- src/config/db.php
+++ src/config/db.php
@@ -13,13 +13,15 @@
       //   return $dbConnection;
       // }
 
-      private $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-      private $server = $url["host"];
-      private $username = $url["user"];
-      private $password = $url["pass"];
-      private $db = substr($url["path"], 1);
       public function connect(){
+        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+        $server = $url["host"];
+        $username = $url["user"];
+        $password = $url["pass"];
+        $db = substr($url["path"], 1);
+        
         $mysql_connect_str = "mysql:host=$this->server;dbname=$this->db;";
         $dbConnection = new PDO($mysql_connect_str, $this->username, $this->password);
         $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
