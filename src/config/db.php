f70fabc812260fdca88009d84188854c419e6cfb
tannergr <tanner@tlgraham.com>
2018-03-01 23:49:43 -0800

    update db config with heroku conn

--- src/config/db.php
+++ src/config/db.php
@@ -1,14 +1,27 @@
 <?php
   class db{
-      // Properties
-      private $dbhost = 'localhost';
-      private $dbuser = 'tanman';
-      private $dbpass = '12345';
-      private $dbname = 'lemons';
+      // // Properties
+      // private $dbhost = 'localhost';
+      // private $dbuser = 'tanman';
+      // private $dbpass = '12345';
+      // private $dbname = 'lemons';
+      //
+      // public function connect(){
+      //   $mysql_connect_str = "mysql:host=$this->dbhost;dbname=$this->dbname;";
+      //   $dbConnection = new PDO($mysql_connect_str, $this->dbuser, $this->dbpass);
+      //   $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      //   return $dbConnection;
+      // }
 
+      $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+      $server = $url["host"];
+      $username = $url["user"];
+      $password = $url["pass"];
+      $db = substr($url["path"], 1);
       public function connect(){
-        $mysql_connect_str = "mysql:host=$this->dbhost;dbname=$this->dbname;";
-        $dbConnection = new PDO($mysql_connect_str, $this->dbuser, $this->dbpass);
+        $mysql_connect_str = "mysql:host=$this->server;dbname=$this->db;";
+        $dbConnection = new PDO($mysql_connect_str, $this->username, $this->password);
         $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $dbConnection;
       }
